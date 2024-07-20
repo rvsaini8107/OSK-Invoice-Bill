fba12df9795595a32c32d991cf94cc416f4f44a4
ShriGanesh <rvsaini8107@gmail.com>
2024-07-20 16:58:50 +0530

    ds

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
+// import React from "react";
+
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
@@ -10,11 +12,7 @@
 export default {
   build: {
     rollupOptions: {
-      external: ['jspdf', 'jspdf-autotable'], // Specify the external module(s) here
+      external: ['jspdf'],
     },
   },
-  plugins: [
-    react(),
-    // Other plugins you're using (e.g., @vitejs/plugin-react)
-  ],
 };
