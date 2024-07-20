b235d0667beb7fefc3444944981b72c71d0b342a
ShriGanesh <rvsaini8107@gmail.com>
2024-07-20 16:49:30 +0530

    abab

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ['jspdf', 'jspdf-autotable'], // Specify the external module(s) here
+    },
+  },
+  plugins: [
+    react(),
+    // Other plugins you're using (e.g., @vitejs/plugin-react)
+  ],
+};
