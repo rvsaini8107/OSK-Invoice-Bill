7c321f9a8a88f937ee1af87e27769cfdd72f0cde
ShriGanesh <rvsaini8107@gmail.com>
2024-07-20 16:34:36 +0530

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 import './App.css'
-import InvoiceBill from './components/InvoiceBill'
+// import InvoiceBill from './components/InvoiceBill'
 import InvoiceBillPc from './components/InvoiceBillPc'
-import NumberToWord from './components/NumberToWord'
+// import NumberToWord from './components/NumberToWord'
 
 function App() {
   const [count, setCount] = useState(0)
